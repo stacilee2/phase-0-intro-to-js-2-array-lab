c392fb378275c091bbf3c35f16274117a10038ae
Staci Rosales <stacileep2@gmail.com>
2022-09-30 14:41:27 -0500

    commit tree 9812efd59f11c98225399d3954fedcdd725453cd

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+} 
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+    return [...cats, "Broom"];
+}
+function prependCat() {
+    return ["Arnold", ...cats];
+}
+function removeLastCat() {
+    const cats = ["Milo", "Otis", "Garfield"];
+    cats.pop();
+    return cats;
+}
+function removeFirstCat() {
+    const cats = ["Milo","Otis", "Garfield"];
+    cats.shift();
+    return cats;
+}
